7ebb4b63f4ab4ffc1d33f5c12862757c11f9580c
DanteMoren <73705466+DanteMoren@users.noreply.github.com>
2021-12-20 15:32:37 +0300

    try fix redoc

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+from django.views.generic import TemplateView
 
 from .views import IngredientsViewSet, RecipeViewSet, TagsViewSet
 
@@ -11,5 +12,10 @@ router.register(r'ingredients', IngredientsViewSet, basename='ingredients')
 router.register(r'recipes', RecipeViewSet, basename='recipes')
 
 urlpatterns = [
+    path(
+        'redoc/',
+        TemplateView.as_view(template_name='redoc.html'),
+        name='redoc'
+    ),
     path('', include(router.urls)),
 ]
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -106,6 +106,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'redoc'),
+]
+
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_URLS_REGEX = r'^/api/.*$'
 
